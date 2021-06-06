47a847acc2558b14bf908f11b4ed95d7f32c8793
atushi <nakanoatushi413@gmail.com>
2021-06-06 22:51:57 +0900

    Initial commit

---
+++ src/Client/Main.java
@@ -0,0 +1,18 @@
+package Client;
+
+
+import Client.UI.Frame;
+
+import java.awt.*;
+
+class Main {
+    public static void main(String[] args) {
+
+        //UI設定と画面表示
+        Frame frame = new Frame("ソケット通信クライアント", new Dimension(500, 500));
+        frame.setVisible(true);
+
+
+
+    }
+}
