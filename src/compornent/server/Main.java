9c9a9830905b9d6ee979d9e5437538817b5a7e0e
nakano <1981070007wv@cyber-u.ac.jp>
2022-02-01 22:04:07 +0900

    PackageStructureChange And guiCompornentAdd

---
+++ src/compornent/server/Main.java
@@ -0,0 +1,8 @@
+package compornent.server;
+
+public class Main {
+    public static void main(String[] args) {
+        compornent.server.ServerC serverC = new ServerC();
+        serverC.serverDstart();
+    }
+}
